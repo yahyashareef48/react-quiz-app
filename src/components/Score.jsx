3b5cfc211812109a225f984e3b48090fecfb72fb
YahyaShareef <yahyashareef48@gmail.com>
2023-06-07 18:36:45 +0530

    some cleaning

--- src/components/Score.jsx
+++
@@ -1,9 +0,0 @@
-export default function Score({ props }) {
-  return (
-    <div>
-      <p>Score: {props.score}</p>
-      <p>Time & Date: {props.time}</p>
-      <p>difficulty: {props.difficulty}</p>
-    </div>
-  );
-}
--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -1,10 +1,15 @@
-import Score from "../components/Score";
 import { v4 as uuidv4 } from "uuid";
 
 export default function ScorePage() {
   const localStorageArr = JSON.parse(localStorage.getItem("score"));
 
-  const scoreElement = localStorageArr.map((data) => <Score key={uuidv4()} props={data} />);
+  const scoreElement = localStorageArr.map((data) => (
+    <div key={uuidv4()}>
+      <p>Score: {data.score}</p>
+      <p>Time & Date: {data.time}</p>
+      <p>difficulty: {data.difficulty}</p>
+    </div>
+  ));
 
   return <main>{scoreElement}</main>;
 }
