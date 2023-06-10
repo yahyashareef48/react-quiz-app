f50dea9d90fafca2ddee7402fdaa3d07f4be9eb8
YahyaShareef <yahyashareef48@gmail.com>
2023-06-10 17:39:20 +0530

    marking right ans and wrong and done

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -3,7 +3,21 @@ import { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid"; // Import the uuidv4 function for generating unique keys
 
 export default function Quiz({ props, handleAns, num, submitted, options }) {
-  const [clicked, setClicked] = useState([false, false, false, false]);
+    const [clicked, setClicked] = useState(() => (
+        JSON.parse(localStorage.getItem("clicked"))
+          ? JSON.parse(localStorage.getItem("clicked"))
+          : [false, false, false, false]
+    ));
+
+    useEffect(() => {
+      localStorage.setItem("clicked", JSON.stringify(clicked));
+      console.log(clicked);
+
+      // Cleanup function
+      return () => {
+        localStorage.removeItem("clicked");
+      };
+    }, [clicked]);
 
   // Generate buttons for each option
   const btn = options.map((ans, index) => {
@@ -22,6 +36,7 @@ export default function Quiz({ props, handleAns, num, submitted, options }) {
         className={`
         ${clicked[index] ? "text-white bg-gray-600" : "text-gray-600"}
         ${props.correct_answer === ans && submitted && "bg-green-500 border-green-500 text-white"}
+        ${props.correct_answer !== ans && clicked[index] && "bg-red-500 border-red-500 text-white"}
         w-auto rounded-full p-4 font-bold border-solid border-2 border-gray-600 m-4
         `}
       >
