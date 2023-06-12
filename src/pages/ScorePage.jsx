9a9e9c69aeae3e0a0578eaa9f6bc574043fae940
YahyaShareef <yahyashareef48@gmail.com>
2023-06-12 16:26:09 +0530

    bug fix of score page

--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from "uuid";
 export default function ScorePage() {
   const localStorageArr = JSON.parse(localStorage.getItem("score")).reverse();
 
-  const scoreElement = localStorageArr.map((data) => (
+  const scoreElement = localStorageArr && localStorageArr.map((data) => (
     <div
       key={uuidv4()}
       className="p-6 shadow-2xl bg-white/60 max-w-md w-full rounded-xl grid gap-2 items-center"
@@ -30,9 +30,11 @@ export default function ScorePage() {
     </div>
   ));
 
-  return (
+  return localStorageArr ? (
     <main className="min-h-screen grid gap-8 p-4 place-items-center bg-gradient-to-tr from-blue-700 via-purple-700 to-orange-700">
       {scoreElement}
     </main>
+  ) : (
+    <h1>There are no scores</h1>
   );
 }
