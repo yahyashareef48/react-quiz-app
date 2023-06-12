3e7f6e5b618224c4b3d3d42a33c04a2c78629c5d
YahyaShareef <yahyashareef48@gmail.com>
2023-06-12 16:34:21 +0530

    score page bug fix 2

--- src/pages/ScorePage.jsx
+++ src/pages/ScorePage.jsx
@@ -1,40 +1,40 @@
 import { v4 as uuidv4 } from "uuid";
 
 export default function ScorePage() {
-  const localStorageArr = JSON.parse(localStorage.getItem("score")).reverse();
+  const localStorageArr = JSON.parse(localStorage.getItem("score"));
 
-  const scoreElement = localStorageArr && localStorageArr.map((data) => (
-    <div
-      key={uuidv4()}
-      className="p-6 shadow-2xl bg-white/60 max-w-md w-full rounded-xl grid gap-2 items-center"
-    >
-      <p className="font-bold text-base">
-        Score: <span className="font-normal">{data.score}</span>
-      </p>
-      <p className="font-bold text-base">
-        Date: <span className="font-normal">{data.time.split("T")[0].replace(/-/g, "/")}</span>
-      </p>
-      <p className="font-bold text-base">
-        difficulty: <span className="font-normal">{data.difficulty}</span>
-      </p>
-      <p className="font-bold text-base">
-        type:{" "}
-        <span className="font-normal">
-          {data.type === "any"
-            ? "Random"
-            : data.type === "boolean"
-            ? "True or False"
-            : "Multiple Chois"}
-        </span>
-      </p>
-    </div>
-  ));
+  const scoreElement =
+    localStorageArr &&
+    localStorageArr.map((data) => (
+      <div
+        key={uuidv4()}
+        className="p-6 shadow-2xl bg-white/60 max-w-md w-full rounded-xl grid gap-2 items-center"
+      >
+        <p className="font-bold text-base">
+          Score: <span className="font-normal">{data.score}</span>
+        </p>
+        <p className="font-bold text-base">
+          Date: <span className="font-normal">{data.time.split("T")[0].replace(/-/g, "/")}</span>
+        </p>
+        <p className="font-bold text-base">
+          difficulty: <span className="font-normal">{data.difficulty}</span>
+        </p>
+        <p className="font-bold text-base">
+          type:{" "}
+          <span className="font-normal">
+            {data.type === "any"
+              ? "Random"
+              : data.type === "boolean"
+              ? "True or False"
+              : "Multiple Chois"}
+          </span>
+        </p>
+      </div>
+    ));
 
-  return localStorageArr ? (
+  return (
     <main className="min-h-screen grid gap-8 p-4 place-items-center bg-gradient-to-tr from-blue-700 via-purple-700 to-orange-700">
-      {scoreElement}
+      {localStorageArr ? scoreElement.reverse() : <h1 className="text-xl font-bold tracking-wide">Scoreboard is empty</h1>}
     </main>
-  ) : (
-    <h1>There are no scores</h1>
   );
 }
